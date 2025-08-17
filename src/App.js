58e1df555145ef0288d3ea74c135bbcd357510ea
C0dName <tortum1996@gmail.com>
2025-08-17 16:52:16 +0300

    заполняем пробелы по js, тудушка

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import Calculator from "./components/calculator/calculator";
-
+import TodoList from "./components/todolist/TodoList";
 
 
 function App() {
+    const [activeComponent, setActiveComponent] = useState(null);
+
   return (
     <div>
-        <Calculator/>
+        <div className='header'>
+            <h3 className='title'>Треня JS</h3>
+            <div className='btnBlock'>
+                <button className='btnNav'
+                        onClick={() => setActiveComponent('calculator')}>
+                    Калькулятор
+                </button>
+                <button className='btnNav'
+                        onClick={() => setActiveComponent('todo')}>
+                    Список задач
+                </button>
+                <button className='btnNav'></button>
+                <button className='btnNav'></button>
+            </div>
+        </div>
+        <div>
+            {activeComponent === 'calculator' && <Calculator />}
+            {activeComponent === 'todo' && <TodoList />}
+        </div>
     </div>
   );
 }
---
+++ src/components/todolist/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoList = () => {
+
+    return (
+        <div>
+            <h1>Список задач</h1>
+            <input type="text"/>
+            
+        </div>
+    );
+};
+
+export default TodoList;
