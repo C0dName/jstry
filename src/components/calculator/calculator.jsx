37269a503c8c1f1259a5f225c14b0fb4e1880287
C0dName <tortum1996@gmail.com>
2025-08-17 12:13:16 +0300

    заполняем пробелы по js

---
+++ src/components/calculator/calculator.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+let num = "call"
+let result = 43
+const isLex = "penis"
+function plus () {
+}
+
+
+const Calculator = () => {
+    return (
+        <div>
+            <h1>{num}</h1>
+            {/*<input type="number" placeholder={"введите число"}/>*/}
+            {/*<input type="number" placeholder={"введите число"}/>*/}
+            {/*<button>+</button>*/}
+            {/*<button>-</button>*/}
+            {/*<h2>{result}</h2>*/}
+        </div>
+    );
+};
+
+export default Calculator;
